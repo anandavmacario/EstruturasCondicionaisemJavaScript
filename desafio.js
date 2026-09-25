f7cf1a0a62f5be1a41ace833bebb67ec26cb2668
anandavmacario <nanda10vm@gmail.com>
2026-09-25 15:33:28 -0300

    AYA

--- desafio.js
+++ desafio.js
@@ -6,3 +6,18 @@
 //"Peso normal"
 //"Sobrepeso"
 //"Obeso"
+
+let peso = 67;
+let altura = 1.55;
+let IMC = peso / (altura * altura);
+
+if (IMC < 18.5) {
+    console.log("Abaixo do peso");
+} else if (IMC >= 18.5 && IMC <= 24.9) {
+    console.log("Peso normal");
+} else if (IMC >= 25.0 && IMC <= 29.9) {
+    console.log("Sobrepeso");
+} else  {
+    console.log("Obeso")
+}
+
